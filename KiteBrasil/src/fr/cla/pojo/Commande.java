eb9cd0ce9fb48204f45dc75bd949bb40cf523546
TheBouls <cedriclamaud6@gmail.com>
2016-08-23 08:52:36 +0200

    Modif apportée pour fonctionner chez Micropole

--- KiteBrasil/src/fr/cla/pojo/Commande.java
+++ KiteBrasil/src/fr/cla/pojo/Commande.java
@@ -1,72 +1,78 @@
 package fr.cla.pojo;
 
 import java.io.Serializable;
-
-import org.joda.time.DateTime;
+import java.util.Date;
 
 public class Commande implements Serializable {
 
-	/**
+    /**
 	 * 
 	 */
-	private static final long serialVersionUID = 555778262696715192L;
+    private static final long serialVersionUID = 555778262696715192L;
 
-	private DateTime commandDate;
-	private String montant;
-	private String modePaiement;
-	private String statutPaiement;
-	private String modeLivraison;
-	private String statutLivraison;
-	
-	private Client client;
-	
-	
-	
-	
-	
-	public DateTime getCommandDate() {
-		return commandDate;
-	}
-	public void setCommandDate(DateTime commandDate) {
-		this.commandDate = commandDate;
-	}
-	public String getMontant() {
-		return montant;
-	}
-	public void setMontant(String montant) {
-		this.montant = montant;
-	}
-	public String getModePaiement() {
-		return modePaiement;
-	}
-	public void setModePaiement(String modePaiement) {
-		this.modePaiement = modePaiement;
-	}
-	public String getStatutPaiement() {
-		return statutPaiement;
-	}
-	public void setStatutPaiement(String statutPaiement) {
-		this.statutPaiement = statutPaiement;
-	}
-	public String getModeLivraison() {
-		return modeLivraison;
-	}
-	public void setModeLivraison(String modeLivraison) {
-		this.modeLivraison = modeLivraison;
-	}
-	public String getStatutLivraison() {
-		return statutLivraison;
-	}
-	public void setStatutLivraison(String statutLivraison) {
-		this.statutLivraison = statutLivraison;
-	}
-	public Client getClient() {
-		return client;
-	}
-	public void setClient(Client client) {
-		this.client = client;
-	}
+    private Date              commandDate;
+    private String            montant;
+    private String            modePaiement;
+    private String            statutPaiement;
+    private String            modeLivraison;
+    private String            statutLivraison;
 
-	
-	
+    private Client            client;
+
+    public Date getCommandDate() {
+        return commandDate;
+    }
+
+    public void setCommandDate( Date commandDate ) {
+        this.commandDate = commandDate;
+    }
+
+    public String getMontant() {
+        return montant;
+    }
+
+    public void setMontant( String montant ) {
+        this.montant = montant;
+    }
+
+    public String getModePaiement() {
+        return modePaiement;
+    }
+
+    public void setModePaiement( String modePaiement ) {
+        this.modePaiement = modePaiement;
+    }
+
+    public String getStatutPaiement() {
+        return statutPaiement;
+    }
+
+    public void setStatutPaiement( String statutPaiement ) {
+        this.statutPaiement = statutPaiement;
+    }
+
+    public String getModeLivraison() {
+        return modeLivraison;
+    }
+
+    public void setModeLivraison( String modeLivraison ) {
+        this.modeLivraison = modeLivraison;
+    }
+
+    public String getStatutLivraison() {
+        return statutLivraison;
+    }
+
+    public void setStatutLivraison( String statutLivraison ) {
+        this.statutLivraison = statutLivraison;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient( Client client ) {
+        this.client = client;
+    }
+
 }
--- KiteBrasil/src/fr/cla/servlets/ControllerCommande.java
+++ KiteBrasil/src/fr/cla/servlets/ControllerCommande.java
@@ -7,8 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.joda.time.DateTime;
-
 import fr.cla.pojo.Client;
 import fr.cla.pojo.Commande;
 import fr.cla.service.ClientForm;
@@ -19,49 +17,47 @@ public class ControllerCommande extends HttpServlet {
     /**
 	 * 
 	 */
-	final String VUE_ERROR = "/WEB-INF/order.jsp";
-	final String VUE = "/WEB-INF/recapOrder.jsp";
-	final String ATT_BEAN_ORDER = "order";
-	final String ATT_BEAN_CLIENT = "client";
-	final String ATT_BEAN_FORM_ORDER = "formOrder";
-	final String ATT_BEAN_FORM_CLIENT = "formClient";
-	
-    private static final long serialVersionUID = 3185938916594774921L;
+    final String              VUE_ERROR            = "/WEB-INF/order.jsp";
+    final String              VUE                  = "/WEB-INF/recapOrder.jsp";
+    final String              ATT_BEAN_ORDER       = "order";
+    final String              ATT_BEAN_CLIENT      = "client";
+    final String              ATT_BEAN_FORM_ORDER  = "formOrder";
+    final String              ATT_BEAN_FORM_CLIENT = "formClient";
+
+    private static final long serialVersionUID     = 3185938916594774921L;
 
     public void doGet( HttpServletRequest request, HttpServletResponse response )
             throws ServletException, IOException {
 
-        
-
         this.getServletContext().getRequestDispatcher( VUE )
                 .forward( request, response );
     }
-    
+
     public void doPost( HttpServletRequest request, HttpServletResponse response )
             throws ServletException, IOException {
-    	
-    	Client client = new Client();
-    	Commande order = new Commande();
-    	ClientForm clientForm = new ClientForm();
-    	CommandForm commandForm = new CommandForm();
-    	
-    	order = commandForm.commandeSaving(request);
-    	client = clientForm.clientSaving(request);
-        
-    	order.setClient(client);
-    	
-    	request.setAttribute(ATT_BEAN_ORDER, order);
-    	request.setAttribute(ATT_BEAN_CLIENT, client);
-    	request.setAttribute(ATT_BEAN_FORM_CLIENT, clientForm);
-    	request.setAttribute(ATT_BEAN_FORM_ORDER, commandForm);
-    	
-    	if (commandForm.getErrors().isEmpty() && clientForm.getErrors().isEmpty() ) {
-    		this.getServletContext().getRequestDispatcher( VUE )
-			.forward( request, response );
-		}else {
-			this.getServletContext().getRequestDispatcher( VUE_ERROR )
-			.forward( request, response );
-			
-		}
+
+        Client client = new Client();
+        Commande order = new Commande();
+        ClientForm clientForm = new ClientForm();
+        CommandForm commandForm = new CommandForm();
+
+        order = commandForm.commandeSaving( request );
+        client = clientForm.clientSaving( request );
+
+        order.setClient( client );
+
+        request.setAttribute( ATT_BEAN_ORDER, order );
+        request.setAttribute( ATT_BEAN_CLIENT, client );
+        request.setAttribute( ATT_BEAN_FORM_CLIENT, clientForm );
+        request.setAttribute( ATT_BEAN_FORM_ORDER, commandForm );
+
+        if ( commandForm.getErrors().isEmpty() && clientForm.getErrors().isEmpty() ) {
+            this.getServletContext().getRequestDispatcher( VUE )
+                    .forward( request, response );
+        } else {
+            this.getServletContext().getRequestDispatcher( VUE_ERROR )
+                    .forward( request, response );
+
+        }
     }
 }
