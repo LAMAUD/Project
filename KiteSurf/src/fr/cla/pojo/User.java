be870635129ed709e1b3a6d04e5887d05d1be933
LAMAUD <cedriclamaud6@gmail.com>
2016-10-05 08:54:45 +0200

    Création des exceptions du DAO

--- KiteSurf/src/fr/cla/pojo/User.java
+++ KiteSurf/src/fr/cla/pojo/User.java
@@ -1,40 +1,59 @@
 package fr.cla.pojo;
 
 import java.io.Serializable;
+import java.sql.Timestamp;
 
 public class User implements Serializable {
 
-    /**
-     * 
-     */
-    private static final long serialVersionUID = 4259541263736557902L;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4259541263736557902L;
 
-    private String            email;
-    private String            pass;
-    private String            nom;
+	private Long id;
+	private String email;
+	private String pass;
+	private String nom;
+	private Timestamp dateInscription;
 
-    public String getEmail() {
-        return email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public void setEmail( String email ) {
-        this.email = email;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public String getPass() {
-        return pass;
-    }
+	public String getPass() {
+		return pass;
+	}
 
-    public void setPass( String pass ) {
-        this.pass = pass;
-    }
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
 
-    public String getNom() {
-        return nom;
-    }
+	public String getNom() {
+		return nom;
+	}
 
-    public void setNom( String nom ) {
-        this.nom = nom;
-    }
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Timestamp getDateInscription() {
+		return dateInscription;
+	}
+
+	public void setDateInscription(Timestamp dateInscription) {
+		this.dateInscription = dateInscription;
+	}
 
 }
