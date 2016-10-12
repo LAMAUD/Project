a3522b7e0a14dc3e03bf47ab366eddac1e562e33
LAMAUD <cedriclamaud6@gmail.com>
2016-10-12 09:10:15 +0200

    DAO Factory créée

---
+++ KiteSurf/src/fr/cla/dao/Exception/DAOConfigurationException.java
@@ -0,0 +1,19 @@
+package fr.cla.dao.Exception;
+
+public class DAOConfigurationException extends RuntimeException {
+
+	/*
+     * Constructeurs
+     */
+    public DAOConfigurationException( String message ) {
+        super( message );
+    }
+
+    public DAOConfigurationException( String message, Throwable cause ) {
+        super( message, cause );
+    }
+
+    public DAOConfigurationException( Throwable cause ) {
+        super( cause );
+    }
+}
