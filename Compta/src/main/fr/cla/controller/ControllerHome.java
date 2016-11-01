cd82f8816e070a804f3fb2943137d4b55f917f04
LAMAUD <cedriclamaud6@gmail.com>
2016-11-01 23:20:59 +0100

    petit comit

---
+++ Compta/src/main/fr/cla/controller/ControllerHome.java
@@ -0,0 +1,12 @@
+package fr.cla.controller;
+
+import javax.servlet.http.HttpServlet;
+
+public class ControllerHome extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4064417889417022067L;
+
+}
